62e869eb84f1115dba966ba42a075cfa447b98f1
Ray Ng <ray.ng@gradeous.com.au>
2019-05-07 14:41:33 +1000

    add scripts and readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# bitbucket-migration-script
+# Bitbucket server repository migration script to Bitbucket Cloud
+
+## This script is to migrate Bitbucket server repository only (not user and permission) to Bitbucket cloud
+
+### Prerequisite
+1. Add a SSH Key into your bitbucket server user account
+2. Have all the repository names that you want to migrate in repo.txt
+
+### Usage
+./bitbucket-migrate.sh repos.txt
+
+### Output
+This script will git clone all the repositories from the list in repo.txt and push it (including all the branches and history) to the new repository in Bitbucket Cloud one by one after creating the repository using the Bitbucket Cloud RESTful Api
+
+### Script steps
+1. git clone a repo using ssh
+2. create a new repo (using the same name) in Bitbucket Cloud using RESTful api
+3. git push to the new repo in Bitbucket Cloud
+4. Clean up the directory
---
+++ bitbucket-migration-script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Author:RayNg@Vibrato
+
+# Usage: ./bitbucket-migrate.sh repos.txt
+#
+# repos.txt should have one repository per line.
+
+echo "Reading $1"
+readonly bsUser=$BITBUCKET_SERVER_USER
+readonly bsServer=$BITBUCKET_SERVER_URL #ssh
+readonly bsSSHPort=$BITBUCKET_SERVER_PORT #ssh
+readonly bsProjectKey=$BITBUCKET_SERVER_PROJECT_KEY
+readonly bcUser=$BITBUCKET_CLOUD_USER
+readonly bcPass=$BITBUCKET_CLOUD_PASS
+readonly bcProjectKey=$BITBUCKET_CLOUD_PROJECT_KEY
+readonly owner=$BITBUCKET_CLOUD_OWNER
+
+while read line
+do
+        repo=$line
+        echo "###"
+        echo "Processing $repo"
+        git clone --bare ssh://${bsUser}@${bsServer}:${bsSSHPort}/${bsProjectKey}/${repo}.git ./${repo}
+        cd $repo
+        echo "Creating repo in Bitbucket"
+        curl -X POST -v -u "$bcUser:$bcPass" "https://api.bitbucket.org/2.0/repositories/$owner/$repo" \
+                     -H "Content-Type: application/json" \
+                     -d '{"has_wiki": true, "is_private": true, "project": {"key": '\"${bcProjectKey}\"'}}'
+        echo "Pushing mirror to bitbucket"
+        echo "git@bitbucket.org:${owner}/${repo}.git"
+        git push --mirror git@bitbucket.org:${owner}/${repo}.git
+        cd ..
+        echo "Removing $repo.git"
+        rm -rf "$repo"
+        echo "Waiting 5 seconds"
+        echo "###"
+        sleep 5;
+
+done < $1
+
+exit
